970d47b00418a83a911221a366862c7949aa5a4d
Gaurav081406 <menaserve08@gmail.com>
2026-07-29 11:19:34 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const path = require('path');
+const logger = require('./src/utils/logger');
+const connectDB = require('./config/database');
+const portScannerRoutes = require('./src/routes/portScanner');
+
+const app = express();
+
+// Security middleware
+app.use(helmet());
+app.use(cors());
+
+// Database connection
+connectDB();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/api/port-scanner', portScannerRoutes);
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
+});
