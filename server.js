7681c5a389445cadead561aa964caec97a92c20f
Gaurav081406 <menaserve08@gmail.com>
2026-07-29 12:34:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,25 @@ const helmet = require('helmet');
 const cors = require('cors');
 const path = require('path');
 const logger = require('./src/utils/logger');
-const { connectDB } = require('./src/utils/database');  // Fixed line
+const { connectDB } = require('./src/utils/database');
 const portScannerRoutes = require('./src/routes/portScanner');
 
 const app = express();
 
-// Security middleware
-app.use(helmet());
+// Security middleware (REMOVE helmet for now or configure it properly)
+// app.use(helmet()); // Comment out or configure CSP header
 app.use(cors());
+
+// Fix: Add specific CSP header to allow React CDN
+app.use((req, res, next) => {
+    res.setHeader('Content-Security-Policy', 
+        "default-src 'self'; " +
+        "script-src 'self' 'unsafe-inline' https://unpkg.com; " +
+        "style-src 'self' 'unsafe-inline'; " +
+        "connect-src 'self'"
+    );
+    next();
+});
 
 // Database connection
 connectDB();
